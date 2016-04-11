05e1f883ed49e803f6711193c19995d4192a309a
huseyinyilmaz93 <huseyin.yilmaz093@gmail.com>
2016-04-11 21:46:11 +0300

    Update ApiEtkinlik.cs

--- AspNetMVC_CSharpTurk/AspNetMVC_CSharpTurk/API/Etkinlik/ApiEtkinlik.cs
+++ AspNetMVC_CSharpTurk/AspNetMVC_CSharpTurk/API/Etkinlik/ApiEtkinlik.cs
@@ -27,11 +27,7 @@ namespace AspNetMVC_CSharpTurk.API.Etkinlik
             string url = "https://etkinlik.io/api/v1/etkinlikler?kategoriId=456";
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-<<<<<<< HEAD
             request.Headers.Add("X-ETKINLIK-TOKEN", "XXX");
-=======
-            request.Headers.Add("X-ETKINLIK-TOKEN", "XXX");
->>>>>>> d78709257007e08fe35366c657d390f113a9234c
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
             Stream resStream = response.GetResponseStream();
